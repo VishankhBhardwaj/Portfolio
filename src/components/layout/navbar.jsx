f3fd0bd1af5cdef5e8d7b26009cafb43c754d7d8
VishankhBhardwaj <pushkar.vikas8@yahoo.com>
2026-05-17 22:52:14 +0530

    fix: update Navbar links to use HTTPS for improved security

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         <div className=" w-full h-[20%] flex flex:row flex-row px-5 py-6 justify-between items-center">
             <div className="border-2 border-white rounded-md  w-[15%] h-[50%]  ml-[5%] mt-[1%] font-bold animate__animated animate__fadeInUp text-[#e4ded7] max-sm:w-[45%] max-sm:h-auto max-sm:py-2 max-sm:ml-[3%] max-sm:mt-0 sm:w-[25%] md:w-[20%] lg:w-[15%] hidden  md:flex md:flex:row justify-center items-center" data-blobity>
                 <Link
-                    href="www.vishankh.dev"
+                    href="https://www.vishankh.dev"
                     target="_blank"
                     aria-label="CONNECT WITH ME">
                     <button>
@@ -14,7 +14,7 @@ export default function Navbar() {
             </div>
             <div className="border-2 border-white rounded-full  w-[7%] h-[50%] flex flex:row justify-center items-center ml-[5%] mt-[1%] font-bold animate__animated animate__fadeInUp text-[#e4ded7] max-sm:w-[12%] max-sm:h-auto max-sm:py-2 max-sm:px-5 max-sm:ml-[3%] max-sm:mt-0 sm:w-[25%] md:w-[20%] lg:w-[15%] md:hidden relative bottom-3" data-blobity>
                 <Link
-                    href="www.vishankh.dev"
+                    href="https://www.vishankh.dev"
                     target="_blank"
                     aria-label="CONNECT WITH ME">
                     <button>
